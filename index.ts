a6399bd09d21346dc22bd796d06b33b7248ff16f
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2021-08-16 14:24:41 -0300

    feat: added package and tests

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+export class ValidationError extends Error {
+  public name = "ValidationError";
+
+  public inner: Array<{ path: string; message: string }> = [];
+
+  public constructor(message: string) {
+    super(message);
+  }
+}
+
+function createValidationError(e: z.ZodError) {
+  const error = new ValidationError(e.message);
+  error.inner = e.errors.map((err) => ({
+    message: err.message,
+    path: err.path.join("."),
+  }));
+
+  return error;
+}
+
+/**
+ * Wrap your zod schema in this function when providing it to Formik's validation schema prop
+ * @param schema The zod schema
+ * @returns An object containing the `validate` method expected by Formik
+ */
+export function toFormikValidationSchema<T>(
+  schema: z.ZodSchema<T>,
+): { validate: (obj: T) => Promise<void> } {
+  return {
+    async validate(obj: T) {
+      try {
+        await Promise.resolve(schema.parse(obj));
+      } catch (err: unknown) {
+        throw createValidationError(err as z.ZodError<T>);
+      }
+    },
+  };
+}
---
+++ tests/index.spec.ts
@@ -0,0 +1,54 @@
+import { z } from "zod";
+import { toFormikValidationSchema, ValidationError } from '../index';
+
+describe("toFormikValidationSchema", () => {
+  it("should pass validate without errors", async () => {
+    // given
+    const object = { name: "mock", age: 32 };
+    const { schema } = makeSut();
+    const { validate } = toFormikValidationSchema(schema);
+    
+    // when
+    const errors = await validate(object);
+
+    // then
+    expect(errors).toEqual(undefined);
+  });
+
+  it("should fail validate with error object", async () => {
+    // given
+    const object = { name: undefined, age: "32" } as any;
+    const { schema } = makeSut();
+    const { validate } = toFormikValidationSchema(schema);
+
+    const error = {} as any;
+    error.inner = [
+      {
+        path:"name",
+        message: "Required",
+      },
+      {
+        path: "age",
+        message: "Expected number, received string",
+      }
+    ]
+
+    // when
+    await expect(
+      validate(object),
+    ).rejects.toMatchObject(error);
+  });
+});
+
+
+
+function makeSut() {
+  const schema = z.object({
+    name: z.string(), // obrigatory name
+    age: z.number().optional(), // optional age
+  });
+
+  return {
+    schema,
+  }
+}
